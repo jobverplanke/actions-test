1c5607ba0556dbc2333fe45dc2333ef2b90d92c2
Job Verplanke <job.verplanke@gmail.com>
2022-03-09 23:41:04 +0100

    chore: update release

--- release.config.js
+++ release.config.js
@@ -3,8 +3,24 @@ module.exports = {
   tagFormat: 'v${version}',
   plugins: [
     ["@semantic-release/commit-analyzer", {
+      releaseRules: [
+        {type: 'breaking', release: 'major'},
+        {type: 'feat', release: 'minor'},
+        {type: 'fix', release: 'patch'},
+        {type: 'chore', release: 'patch'},
+      ],
       parserOpts: {
         noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+      },
+    }],
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+      presetConfig: {
+        types: [
+          {type: 'feat', section: 'Features'},
+          {type: 'fix', section: 'Fixes'},
+          {type: 'chore', section: 'Chores'},
+        ],
       }
     }],
     "@semantic-release/github"
